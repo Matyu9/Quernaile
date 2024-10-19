3fb17045a5587caf4aae132dfe54ae75ac961d37
Matyu9 <59774749+Matyu9@users.noreply.github.com>
2024-10-19 23:15:48 +0200

    LE DISK NE MARCHE PAS OSKOUUUUURRRRRRRRRRRR

--- Bootloader/boot_sect_read_disk.asm
+++ Bootloader/boot_sect_read_disk.asm
@@ -1,26 +1,64 @@
 ; A boot sector that prints strings with function.
 
+BITS 16
 [ org 0x7c00 ]
-mov bx, HELLO_MSG
+
+; Initialisation des segments
+xor ax, ax
+mov ds, ax
+mov es, ax
+
+mov [BOOT_DRIVE], dl ; Store boot drive un dl for later
+
+mov bp, 0x8000 ; Move the stack out of the way
+mov sp, bp     ; At 0x8000
+
+mov bx, LOAD_MSG
 call print_string
+
+; Préparation des paramètres de lecture du disque
+mov ax, 0x0900         ; Segment value
+mov es, ax             ; ES = 0x0900
+xor bx, bx             ; BX = 0, donc ES:BX pointe sur 0x90000
+mov dh, 2              ; Nombre de secteurs à lire
+mov dl, [BOOT_DRIVE]   ; Disque source
+mov ch, 0              ; Cylindre 0
+mov cl, 2              ; Commencer au secteur 2 (après le boot sector)
+mov al, dh             ; Nombre de secteurs dans AL aussi
+mov ah, 0x02           ; Fonction de lecture BIOS
+
+call disk_load
+
+mov dx, [es:bx] ; Print the first word stored at 0x9000 (0xdada)
+call print_hex
+
+mov dx, [es:bx + 512] ; Print the first word of the 2nd sector (0xface)
+call print_hex
 
 mov bx, GOODBYE_MSG
 call print_string
 
-mov dx, 0xFFFF
-call print_hex
-
 jmp $
 
 ; Variables
-HELLO_MSG:
-    db 'Hello, World From an Assembly Function!', 13, 10, 0
+LOAD_MSG:
+    db 'Loading the disk...', 13, 10, 0
 
 GOODBYE_MSG:
-    db 'Goodbye!', 13, 10, 0
+    db 'Goodbye!', 13, 10, "All is good I think" , 13, 10, 0
+
+BOOT_DRIVE:
+    db 0
 
 %include "../func/print_asm/print_string.asm"
+%include "../func/print_asm/print_char.asm"
 %include "../func/print_asm/print_hex.asm"
+%include "../func/disk_asm/read_disk.asm"
 
 times 510 -( $ - $$ ) db 0
 dw 0xaa55
+
+
+; Load data outside of the 512 byte boot sector
+times 256 dw 0xdada
+times 256 dw 0xface
--- func/disk_asm/read_disk.asm
+++ func/disk_asm/read_disk.asm
@@ -1,27 +1,34 @@
 disk_load:
-    push dx        ; Store DX on stack to know how many sector we want if we want to
+    pusha
+    push dx              ; Sauvegarder DX qui contient DH (nb secteurs) et DL (drive number)
 
-    mov ah , 0x02  ; BIOS read sector function
-    mov al, dh     ; Read DH sectors
-    mov ch, 0x00   ; Select cylinder 0
-    mov dh, 0x00   ; Select head 0
-    mov cl, 0x02   ; Select sector 2 (after boot sector)
+    int 0x13             ; BIOS Interrupt
+    jc disk_error        ; jump if error (if carry flag set)
 
-    int 0x13       ; BIOS Interrupt
+    pop dx               ; Restaurer DX pour avoir le nombre de secteurs dans DH
+    cmp al, dh           ; Comparer avec le nombre de secteurs lus (DH contient le nombre voulu)
+    jne sectors_error    ; Error message si différent
 
-
-
-    jc disk_error  ; jump if error (if carry flag set)
-
-    pop dx         ; Restore DX
-    cmp dh, al     ; If sectors read (AL) != sectors expected (DH)
-    jne disk_error ; Error message
+    popa
+    clc                  ; Clear carry flag (pas d'erreur)
+    ret
 
 disk_error:
     mov bx, DISK_ERROR_MSG
+    call print_string
+
+    mov dx, ax           ; Afficher le code d'erreur
+    call print_hex
+    jmp $
+
+sectors_error:
+    mov bx, SECTORS_ERROR_MSG
     call print_string
     jmp $
 
 ; Variables
 DISK_ERROR_MSG:
-    db "Disk read error!", 0
+    db "Disk read error!", 13, 10, 0
+
+SECTORS_ERROR_MSG:
+    db "Incorrect number of sectors read!", 13, 10, 0
